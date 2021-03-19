3972824903ead4c000ca6109f43af446463c24a1
Mahmoud Morsy <mahmoudahmedmorsy@gmail.com>
2021-03-19 08:18:31 +0200

    Adding the project files

---
+++ SportsApp/SportsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SportsApp
+//
+//  Created by Mahmoud Morsy on 19/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
