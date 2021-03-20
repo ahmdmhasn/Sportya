3123158cfd2ea90eaf0b294364be07e19fc0d66a
Mahmoud Morsy <mahmoudahmedmorsy@gmail.com>
2021-03-21 01:01:35 +0200

    Adding SportsCollectionView and Cell

--- SportsApp/SportsApp/SportsViewController.swift
+++ SportsApp/SportsApp/SportsViewController.swift
@@ -9,17 +9,21 @@ import UIKit
 
 class SportsViewController: UIViewController {
 
+    @IBOutlet weak var backGroundImage: UIImageView!
     @IBOutlet weak var sportsCollectionView: UICollectionView!
+    var backgroundImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         sportsCollectionView.delegate = self
         sportsCollectionView.dataSource = self
         sportsCollectionView.register(UINib(nibName: "SportsCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: String(describing: SportsCollectionViewCell.self))
-
         
+        backGroundImage.image = UIImage(named: "background8")
+        backGroundImage.contentMode = .scaleAspectFill
     }
     
+
 }
 
 
@@ -32,19 +36,16 @@ extension SportsViewController: UICollectionViewDelegate,UICollectionViewDataSou
         guard let cell = sportsCollectionView.dequeueReusableCell(withReuseIdentifier: String(describing: SportsCollectionViewCell.self), for: indexPath) as? SportsCollectionViewCell else {
             return UICollectionViewCell()
         }
+        cell.contentView.layer.borderWidth = 4
+        cell.contentView.layer.cornerRadius = 8
+        
         
         return cell
     }
     
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        let width = sportsCollectionView.bounds.width/2
-//        let height = width - 50
-        let width = (view.frame.size.width - 20) / 2
-        
-        return CGSize(width: width, height: width)
-        
-     
+        let size = (view.frame.size.width - 30) / 2
+        return CGSize(width: size, height: size)
     }
-    
-    
 }
